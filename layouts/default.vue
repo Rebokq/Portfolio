7242f2bdb4d2e8095b7bf3596b47c81d7a2ad765
Rebokq <louiskabore1998@gmail.com>
2023-10-18 18:08:04 +0200

    setup route

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex h-screen bg-gray-400">
+
+        <div>
+            <slot />
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <a class="text-xl text-red-400">Home</a>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
